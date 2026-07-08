41e867fec2460562476f0712af3ceaaacfe5166a
Lee Ji Hwan <smiilliin00@gmail.com>
2026-07-08 18:26:25 +0900

    feat: CILS 더미 추가

--- scripts/dummyCards.ts
+++ scripts/dummyCards.ts
@@ -1,5 +1,6 @@
 import type { CardData, DeckInfo } from "../src/types/deck";
 import jlptN35 from "./jlpt_n35.json";
+import cilsA1B1 from "./cils_a1b1.json";
 
 // leftpad id
 const leftpad = (str: string, len: number) => {
@@ -14,6 +15,11 @@ const jlptCards: CardData[] = jlptN35.map((card, index) => ({
   id: `jp-n35-${leftpad(index.toString(), 6)}`,
   index: index,
 }));
+const cilsCards: CardData[] = cilsA1B1.map((card, index) => ({
+  ...card,
+  id: `cils-a1b1-${leftpad(index.toString(), 6)}`,
+  index: index,
+}));
 
 export const dummyDecks: { info: DeckInfo; cards: CardData[] }[] = [
   {
@@ -26,5 +32,16 @@ export const dummyDecks: { info: DeckInfo; cards: CardData[] }[] = [
       cardcount: jlptCards.length,
     },
     cards: jlptCards,
+  },
+  {
+    info: {
+      id: "cils-a1b1",
+      title: "CILS A1-B1",
+      description: "CILS A1-B1 수준의 일본어 표현을 학습합니다.",
+      language: "Italian",
+      level: "A1-B1",
+      cardcount: cilsCards.length,
+    },
+    cards: cilsCards,
   },
 ];
